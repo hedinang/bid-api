0577605af585251ab420ee21258194bddefc01fb
hedinang <tridunga1@gmail.com>
2024-12-17 20:41:58 +0700

    init project

---
+++ src/main/java/com/example/bid_api/BidApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bid_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bid_api/BidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bid_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
